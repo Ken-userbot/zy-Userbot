492b3ae90fd721dc8b54e8fe58ab1d91194703f6
Rmj <gabriellreyco@gmail.com>
2025-03-07 08:08:33 +0700

    Update ping.py

--- AyiinXd/modules/ping.py
+++ AyiinXd/modules/ping.py
@@ -61,7 +61,7 @@ async def _(ping):
     await Ayiin.edit("🥵")
     sleep(3)
     await Ayiin.edit(
-        f"**㋡ 𝙺𝙰𝚉𝚄-𝚄𝚂𝙴𝚁𝙱𝙾𝚃 ㋡**\n\n"
+        f"**㋡ roemahjaseb-𝚄bot ㋡**\n\n"
         f"㋡ **𝙿𝙸𝙽𝙶𝙴𝚁 :** `%sms`\n"
         f"㋡ **𝚄𝙿𝚃𝙸𝙼𝙴 :** `{uptime}` \n"
         f"㋡ **𝙾𝚆𝙽𝙴𝚁 :** [{user.first_name}](tg://user?id={user.id})" % (duration)
